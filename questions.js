9679bf7e98b58597141134de4869798dc0879f6a
Joshua Robson <joshua.s.robson@icloud.com>
2023-03-05 19:26:59 +0000

    modified:   Questions2.html 	modified:   questions.js

--- questions.js
+++ questions.js
@@ -168,20 +168,173 @@ function generateType7Question() {
   }
 
   function numberRotation() {
-    let numbers = Array.from({ length: Math.floor(Math.random() * 10) + 1 }, () => Math.floor(Math.random() * 100));
-    let rotateBy = Math.floor(Math.random() * numbers.length);
-    let question = `${rotateBy} % ${numbers.join(" ")}`;
-    let answer = numbers.slice(rotateBy).concat(numbers.slice(0, rotateBy)).join(" ");
-    return { question: question, answer: answer };
+    const n = Math.floor(Math.random() * 10) + 1; // Generate a random number between 1 and 10
+    const arr = Array.from({length: 5}, () => Math.floor(Math.random() * 100)); // Generate an array of 5 random numbers between 0 and 99
+    const rotatedArr = [...arr.slice(n), ...arr.slice(0, n)]; // Rotate the array by n elements
+    return `${n} % ${arr.join(" ")} = ${rotatedArr.join(" ")}`; // Return the question as a string
   }
 
   function binarySequenceCreation() {
-    let numbers = Array.from({ length: 4 }, () => Math.floor(Math.random() * 10));
-    let baseNumber = Math.max(...numbers) + 1;
-    let binarySequence = numbers.map((val) => {
-      return val < baseNumber ? 0 : 1;
-    });
-    let question = `${baseNumber}: ${numbers.join(" ")}`;
-    let answer = binarySequence.join(" ");
-    return { question: question, answer: answer };
+    const numBefore = Math.floor(Math.random() * 100); // Generate a random number before the ":"
+    const numArray = []; // Create an empty array to store the numbers after the ":"
+    const numCount = Math.floor(Math.random() * 10) + 1; // Generate a random number of numbers to include after the ":"
+  
+    // Generate the numbers after the ":" and make sure they are not equal to the number before
+    for (let i = 0; i < numCount; i++) {
+      let num = numBefore;
+      while (num === numBefore) {
+        num = Math.floor(Math.random() * 100);
+      }
+      numArray.push(num);
+    }
+  
+    // Generate the binary sequence based on the numbers after the ":"
+    let binarySequence = "";
+    for (let i = 0; i < numArray.length; i++) {
+      if (numArray[i] < numBefore) {
+        binarySequence += "0";
+      } else {
+        binarySequence += "1";
+      }
+    }
+  
+    // Return the question and the binary sequence
+    return `${numBefore} : ${numArray.join(" ")} = ${binarySequence}`;
   }
+
+  function generateLetters() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const letterCount = Math.floor(Math.random() * 6) + 3; // generate a random letter count between 3 and 8
+    const n = Math.floor(Math.random() * 3) + 1; // Generate a random number between 1 and 10
+    const arr = Array.from({length: 5}, () => Math.floor(Math.random() * letterCount)+1);
+    const rotatedArr = [...arr.slice(n), ...arr.slice(0, n)]; // Rotate the array by n elements
+    //const strrotatedArr = rotatedArr.join(' ')
+    const numCount = Math.floor(Math.random() * 3) + 1; // generate a random number count between 1 and 3
+    const positions = [];
+    
+    // generate the positions array with random numbers between 1 and the letter count
+    for (let i = 0; i < numCount; i++) {
+      positions.push(Math.floor(Math.random() * letterCount) + 1);
+    }
+    
+    // sort the positions array in ascending order
+    //positions.sort((a, b) => a - b);
+    //arr.sort((a, b) => a - b);
+    // generate the letters string with random letters
+    let letterString = "";
+    for (let i = 0; i < letterCount; i++) {
+      letterString += letters.charAt(Math.floor(Math.random() * letters.length));
+    }
+    
+    // generate the answer string based on the positions array
+    let answerString = "";
+    for (let i = 0; i < rotatedArr.length; i++) {
+      answerString += letterString.charAt(rotatedArr[i] - 1);
+    }
+    
+    // generate the question string in the format of (LETTERS)[NUMBERS]
+    //const questionString = `(${letterString})[${positions.join(' ')}]`;
+    const questionString = `(${letterString})[${n} % ${arr.join(" ")}]`;
+    
+    // return the question and answer strings as a single string
+    return `${questionString} = ${answerString}`;
+  }
+  
+  function generatechangeorderQuestion() {
+  // Generate a random number between -10 and 10 (inclusive) for n
+ 
+
+  // Generate a random number between 1 and 10 (inclusive) for x
+  const x = Math.floor(Math.random() * 10) + 1;
+  const n = (x - Math.floor(Math.random() * x))*(-1); 
+  // Generate an array of numbers from 1 to x
+  const arr = Array.from({ length: x }, (_, i) => i + 1);
+
+  // Shuffle the array randomly
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  // Determine the absolute value of n
+  const absN = Math.abs(n);
+
+  // Ensure that absN is less than or equal to the length of arr
+  const effectiveN = Math.min(absN, arr.length);
+
+  // Select the output numbers based on the value of n
+  let output;
+  if (n < 0) {
+    output = arr.slice(-effectiveN);
+  } else {
+    output = arr.slice(0, effectiveN);
+  }
+
+  // Combine the question string and output numbers into a single string
+  const question = `${n} @ (!${x})`;
+  const outputString = output.join(' ');
+  const result = `${question} = ${outputString}`;
+
+  return result;
+}
+
+function cumulativetableFilling() {
+  const maxRows = 2;
+  const maxCols = 2;
+  const numRows = Math.ceil(Math.random() * maxRows);
+  const numCols = Math.ceil(Math.random() * maxCols);
+
+  const table1Nums = generateRandomNums(numRows, numCols);
+  const table2Nums = generateRandomNums(numRows, numCols);
+  const table3Nums = addTables(table1Nums, table2Nums);
+
+  const question = `(${numRows} ${numCols} ? ${table1Nums}) + (${numRows} ${numCols} ? ${table2Nums}) =`;
+  const answer = `<br>${table3Nums}`;
+
+  return [question, answer];
+}
+
+function generateRandomNums(numRows, numCols) {
+  const numValues = Math.floor(Math.random() * 5) + 3;
+  const nums = [];
+  for (let i = 0; i < numValues; i++) {
+    nums.push(Math.floor(Math.random() * 9) + 1);
+  }
+
+  let tableNums = "";
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      const numIndex = i * numCols + j;
+      const num = nums[numIndex % numValues];
+      tableNums += num;
+      if (j < numCols - 1) {
+        tableNums += " ";
+      }
+    }
+    if (i < numRows - 1) {
+      tableNums += ",";
+    }
+  }
+  return tableNums;
+}
+
+function addTables(table1Nums, table2Nums) {
+  const table1Rows = table1Nums.split(",").map(row => row.split(" "));
+  const table2Rows = table2Nums.split(",").map(row => row.split(" "));
+  const numRows = table1Rows.length;
+  const numCols = table1Rows[0].length;
+  const table3Rows = [];
+
+  for (let i = 0; i < numRows; i++) {
+    const table3Row = [];
+    for (let j = 0; j < numCols; j++) {
+      const num1 = parseInt(table1Rows[i][j]);
+      const num2 = parseInt(table2Rows[i][j]);
+      const sum = isNaN(num1) || isNaN(num2) ? "" : num1 + num2;
+      table3Row.push(sum);
+    }
+    table3Rows.push(table3Row.join(" "));
+  }
+
+  return table3Rows.join("<br> ");
+}
